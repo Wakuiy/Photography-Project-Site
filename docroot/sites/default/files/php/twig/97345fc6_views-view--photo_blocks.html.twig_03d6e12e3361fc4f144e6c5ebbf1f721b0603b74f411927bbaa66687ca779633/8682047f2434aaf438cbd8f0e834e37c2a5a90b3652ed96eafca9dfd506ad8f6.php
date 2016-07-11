db6be8c5b9dd1389b9ed25507f0d912435c9efa8
Wakuiy <jake.ryan@accessadvertising.co.uk>
2016-07-11 11:40:26 +0100

    Added paragrahps and generated files

---
+++ docroot/sites/default/files/php/twig/97345fc6_views-view--photo_blocks.html.twig_03d6e12e3361fc4f144e6c5ebbf1f721b0603b74f411927bbaa66687ca779633/8682047f2434aaf438cbd8f0e834e37c2a5a90b3652ed96eafca9dfd506ad8f6.php
@@ -0,0 +1,138 @@
+<?php
+
+/* themes/Photographie/templates/views-view--photo_blocks.html.twig */
+class __TwigTemplate_e7dd9129189742280ea90f9cfaccf7084161c145564547d41511275253fc2b69 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 39);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 35
+        echo "
+<div class='photo-block>' id='home-photo-blocks'>
+  ";
+        // line 37
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title"]) ? $context["title"] : null), "html", null, true));
+        echo "
+
+  ";
+        // line 39
+        if ((isset($context["rows"]) ? $context["rows"] : null)) {
+            // line 40
+            echo "    <div class=\"view-content\">
+      ";
+            // line 41
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["rows"]) ? $context["rows"] : null), "html", null, true));
+            echo "
+    </div>
+
+  ";
+        } elseif (        // line 44
+(isset($context["empty"]) ? $context["empty"] : null)) {
+            // line 45
+            echo "    <div class=\"view-empty\">
+      ";
+            // line 46
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["empty"]) ? $context["empty"] : null), "html", null, true));
+            echo "
+    </div>
+  ";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/Photographie/templates/views-view--photo_blocks.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  68 => 46,  65 => 45,  63 => 44,  57 => 41,  54 => 40,  52 => 39,  47 => 37,  43 => 35,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Default theme implementation for main view template.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: Remaining HTML attributes for the element.*/
+/*  * - css_name: A css-safe version of the view name.*/
+/*  * - css_class: The user-specified classes names, if any.*/
+/*  * - header: The optional header.*/
+/*  * - footer: The optional footer.*/
+/*  * - rows: The results of the view query, if any.*/
+/*  * - empty: The content to display if there are no rows.*/
+/*  * - pager: The optional pager next/prev links to display.*/
+/*  * - exposed: Exposed widget form/info to display.*/
+/*  * - feed_icons: Optional feed icons to display.*/
+/*  * - more: An optional link to the next page of results.*/
+/*  * - title: Title of the view, only used when displaying in the admin preview.*/
+/*  * - title_prefix: Additional output populated by modules, intended to be*/
+/*  *   displayed in front of the view title.*/
+/*  * - title_suffix: Additional output populated by modules, intended to be*/
+/*  *   displayed after the view title.*/
+/*  * - attachment_before: An optional attachment view to be displayed before the*/
+/*  *   view content.*/
+/*  * - attachment_after: An optional attachment view to be displayed after the*/
+/*  *   view content.*/
+/*  * - dom_id: Unique id for every view being printed to give unique class for*/
+/*  *   Javascript.*/
+/*  **/
+/*  * @see template_preprocess_views_view()*/
+/*  **/
+/*  * @ingroup templates*/
+/*  *//* */
+/* #}*/
+/* */
+/* <div class='photo-block>' id='home-photo-blocks'>*/
+/*   {{ title }}*/
+/* */
+/*   {% if rows %}*/
+/*     <div class="view-content">*/
+/*       {{ rows }}*/
+/*     </div>*/
+/* */
+/*   {% elseif empty %}*/
+/*     <div class="view-empty">*/
+/*       {{ empty }}*/
+/*     </div>*/
+/*   {% endif %}*/
+/* */
